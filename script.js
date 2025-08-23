6d6c471dda25d8227c7e6e8048e5bd4ef6d4a81a
Kristian Tufte Sterud <kristian0805@hotmail.com>
2025-08-23 14:55:38 +0200

    Functioning calculator without keyboard support

--- script.js
+++ script.js
@@ -1,16 +1,96 @@
 const display = document.querySelector(".display");
-
 const digits = document.querySelector(".digits");
+const operators = document.querySelector(".operators");
+
+let firstOperand = null;
+let secondOperand = null;
+let operation = null;
+let shouldResetDisplay = false;
+let isErrorState = false;
 
 digits.addEventListener("click", (event) => {
-  let digit = event.target;
-  currentDisplay = display.textContent;
-  if (currentDisplay == 0) {
-    display.textContent = digit.textContent;
+  if (isErrorState) {
+    clearCalculator();
+    display.textContent = 0;
+    isErrorState = false;
+  }
+  const digit = event.target.textContent;
+  if (digit === "." && display.textContent.match(/\./) !== null) {
+    return;
+  }
+  if (shouldResetDisplay) {
+    display.textContent = digit;
+    shouldResetDisplay = false;
   } else {
-    display.textContent += digit.textContent;
+    display.textContent =
+      display.textContent === "0" ? digit : display.textContent + digit;
   }
 });
+
+operators.addEventListener("click", (event) => {
+  if (isErrorState) {
+    clearCalculator();
+    display.textContent = "0";
+    isErrorState = false;
+  }
+  let operator = event.target.textContent;
+
+  if (operator === "Clear") {
+    clearCalculator();
+    display.textContent = "0";
+    return;
+  }
+  if (operator === "Back") {
+    display.textContent = 0;
+    return;
+  }
+
+  if (operator === "=") {
+    if (firstOperand !== null && operation && !shouldResetDisplay) {
+      secondOperand = Number(display.textContent);
+      const result = operate(firstOperand, operation, secondOperand);
+
+      if (!isFinite(result)) {
+        display.textContent = "You can't divide by 0 dummy";
+        clearCalculator();
+        isErrorState = true;
+        return;
+      } else {
+        display.textContent = result;
+        firstOperand = result;
+        operation = null;
+      }
+    }
+    shouldResetDisplay = true;
+    return;
+  }
+
+  if (firstOperand !== null && operation && !shouldResetDisplay) {
+    secondOperand = Number(display.textContent);
+    const result = operate(firstOperand, operation, secondOperand);
+
+    if (!isFinite(result)) {
+      display.textContent = "You can't divide by 0 dummy";
+      clearCalculator();
+      isErrorState = true;
+      return;
+    } else {
+      display.textContent = result;
+      firstOperand = result;
+    }
+  } else {
+    firstOperand = Number(display.textContent);
+  }
+  operation = operator;
+  shouldResetDisplay = true;
+});
+
+function clearCalculator() {
+  firstOperand = null;
+  secondOperand = null;
+  operation = null;
+  shouldResetDisplay = false;
+}
 
 function add(a, b) {
   return a + b;
